759e9f1fe7b2e09b865c34ad230a98edadb989c3
Tiago Goulart <tiagogoulart3012@gmail.com>
2024-04-08 14:43:55 -0300

    exercicio resolvido

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+  test("Adição de dois comentários", () => {
+    render(<PostComment />);
+    fireEvent.change(screen.getByTestId("comentario-textarea"), {
+      target: {
+        value: "Teste comentário 1",
+      },
     });
+    fireEvent.click(screen.getByTestId("comentario-button"));
+
+    fireEvent.change(screen.getByTestId("comentario-textarea"), {
+      target: {
+        value: "Teste comentário 2",
+      },
+    });
+    fireEvent.click(screen.getByTestId("comentario-button"));
+
+    expect(screen.getAllByTestId("comentario-li")).toHaveLength(2);
+  });
 });
